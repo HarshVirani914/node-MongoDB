6f7988a2ca0bce2a7bbd2b53eef30e3374253d82
harsh virani <harshvirani.91@gmail.com>
2022-11-15 13:43:10 +0530

    started-server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
 
-export const app = express()
+export const app = express();
 
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-export const start = () => {}
+app.get("/data", (req, res) => {
+  res.send({ msg: "Hello World!" });
+});
+
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+};
